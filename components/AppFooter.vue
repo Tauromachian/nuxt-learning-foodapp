b2161f192d5c1970206f1b3b7815743391ffff9e
Jose Garcia <jogarcia@tuta.io>
2021-04-25 11:00:02 -0400

    Make footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <p>Copyright &copy; 2020 Food app</p>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  width: 100%;
+  left: 0;
+  bottom: 0;
+  color: white;
+  text-align: center;
+  padding: 1em;
+}
+</style>
