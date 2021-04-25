abe2e25853bd52869406003006831c1dd8ed30ff
Jose Garcia <jogarcia@tuta.io>
2021-04-25 10:18:27 -0400

    Make hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <main class="container">
+    <AppHeader />
     <h1>Hi from homepage!</h1>
   </main>
 </template>
 
 <script>
-export default {};
+import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader,
+  },
+};
 </script>
 
 <style>
