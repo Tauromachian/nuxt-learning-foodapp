cbc10baf7aeeeb1cd7ad7e93ff1db16b1d1d3178
Jose Garcia <jogarcia@tuta.io>
2022-03-27 13:08:28 -0400

    Change base route

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
   router: {
-    base: 'nuxt-learning-foodapp',
+    base: '/nuxt-learning-foodapp/dist',
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
